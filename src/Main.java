12c09a1bcb8dd123f4db5441ad924d40cf20c9e7
Lazar Marian <lazar.marian1528@gmail.com>
2023-10-08 14:52:02 +0300

    Initial commit dasd

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,13 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
-    //adasdasdaxsds
+        //adasdasdaxsds
         //adasd
         for (int i = 1; i <= 5; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
+        }
     }
 }
